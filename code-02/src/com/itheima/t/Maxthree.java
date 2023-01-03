6f8cc275a8067f6968925dec435f9c0292a14a37
Azu-mio <misakaovo@163.com>
2023-01-03 14:00:36 +0800

    求最大数

--- code-02/src/com/itheima/t/Maxthree.java
+++ code-02/src/com/itheima/t/Maxthree.java
@@ -7,6 +7,12 @@ public class Maxthree {
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入第一个数");
         int a = sc.nextInt();
+        System.out.println("请输入第二个数");2
+        int b = sc.nextInt();
+        System.out.println("请输入第三个数");
+        int c = sc.nextInt();
+        int temp = a > b ? a : b;
+        System.out.println(temp>c ? temp : c);
 
     }
 }
