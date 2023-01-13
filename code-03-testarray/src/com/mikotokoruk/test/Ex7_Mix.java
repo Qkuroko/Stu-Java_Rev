d0380624329648ec1a491d25e1bcd1a6f37c440c
Azu-mio <misakaovo@163.com>
2023-01-14 01:40:14 +0800

    打乱数据:这里明白随机数生成设计的方便了

---
+++ code-03-testarray/src/com/mikotokoruk/test/Ex7_Mix.java
@@ -0,0 +1,20 @@
+package com.mikotokoruk.test;
+
+import java.util.Random;
+
+public class Ex7_Mix {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        Random r = new Random();
+        for (int i = 0; i<5; i++){
+//            int number = r.nextInt(0,5);//生成0-4的随机索引
+            int number = r.nextInt(arr.length);//太妙了
+            int temp = arr[i];
+            arr[i] = arr[number];
+            arr[number] = temp;
+        }
+        for (int j = 0; j<arr.length; j++){
+            System.out.print(arr[j]+" ");
+        }
+    }
+}
