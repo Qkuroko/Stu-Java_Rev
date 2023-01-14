02d389ebf99a98c33d47d59f790e151e44e723cf
Azu-mio <misakaovo@163.com>
2023-01-14 19:43:20 +0800

    打乱数据:这里明白随机数生成设计的方便了

--- code-03-testarray/src/com/mikotokoruk/test/Ex7_Mix.java
+++ code-03-testarray/src/com/mikotokoruk/test/Ex7_Mix.java
@@ -8,7 +8,7 @@ public class Ex7_Mix {
         Random r = new Random();
         for (int i = 0; i<5; i++){
 //            int number = r.nextInt(0,5);//生成0-4的随机索引
-            int number = r.nextInt(arr.length);//太妙了
+            int number = r.nextInt(arr.length);//生成0-4的随机索引
             int temp = arr[i];
             arr[i] = arr[number];
             arr[number] = temp;
