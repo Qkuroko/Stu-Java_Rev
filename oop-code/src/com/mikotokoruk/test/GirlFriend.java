8d77c5efe00352e4bf63524b43ed290c6478517b
Azu-mio <misakaovo@163.com>
2023-01-15 16:14:48 +0800

    对象

---
+++ oop-code/src/com/mikotokoruk/test/GirlFriend.java
@@ -0,0 +1,16 @@
+package com.mikotokoruk.test;
+
+public class GirlFriend {
+    //属性
+    String name;
+    int age;
+
+
+    //行为
+    public static void eat(){
+        System.out.println("GirlFriend is eating.");
+    }
+    public static void sleep(){
+        System.out.println("GirlFriend is sleeping.");
+    }
+}
---
+++ oop-code/src/com/mikotokoruk/test/TestGirlFriend.java
@@ -0,0 +1,10 @@
+package com.mikotokoruk.test;
+
+public class TestGirlFriend {
+    public static void main(String[] args) {
+        GirlFriend mikoto = new GirlFriend();
+        mikoto.name = "mikoto";
+        System.out.println(mikoto.name);
+        mikoto.eat();
+    }
+}
