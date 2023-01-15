326383886c7e82c75084bb83a543825848760bcb
mikotokoruk <misakaovo@163.com>
2023-01-15 23:21:37 +0800

    pgt插件一键生成标准Javabea

---
+++ oop-code/src/com/mikotokoruk/test/Student.java
@@ -0,0 +1,106 @@
+package com.mikotokoruk.test;
+
+public class Student {
+    //用PTG插件可实现快速创建标准Javabean类  鼠标右键
+    private String name;
+    private String password;
+    private String email;
+    private String gender;
+    private int age;
+
+
+    public Student() {
+    }
+
+    public Student(String name, String password, String email, String gender, int age) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * 获取
+     * @return gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * 设置
+     * @param gender
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "Student{name = " + name + ", password = " + password + ", email = " + email + ", gender = " + gender + ", age = " + age + "}";
+    }
+}
