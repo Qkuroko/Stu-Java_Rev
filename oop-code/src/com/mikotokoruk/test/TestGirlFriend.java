66335cb56aa33538c2e10fa4bce600510ad343b3
Azu-mio <misakaovo@163.com>
2023-01-15 16:52:08 +0800

    对象

--- oop-code/src/com/mikotokoruk/test/TestGirlFriend.java
+++ oop-code/src/com/mikotokoruk/test/TestGirlFriend.java
@@ -4,6 +4,7 @@ public class TestGirlFriend {
     public static void main(String[] args) {
         GirlFriend mikoto = new GirlFriend();
         mikoto.name = "mikoto";
+        //11
         System.out.println(mikoto.name);
         mikoto.eat();
     }
