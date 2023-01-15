c807ac34aba165a6be8f553f6ccbcff0b1fa6f04
mikotokoruk <misakaovo@163.com>
2023-01-15 17:11:48 +0800

    对象

--- oop-code/src/com/mikotokoruk/test/TestGirlFriend.java
+++ oop-code/src/com/mikotokoruk/test/TestGirlFriend.java
@@ -4,7 +4,7 @@ public class TestGirlFriend {
     public static void main(String[] args) {
         GirlFriend mikoto = new GirlFriend();
         mikoto.name = "mikoto";
-        //111111122
+        //1111111224433
         System.out.println(mikoto.name);
         mikoto.eat();
     }
